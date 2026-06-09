9923e2f9e78b3ab8f368ed1a61048c0a55af0d4d
Lokosek <36383101+Lokosek@users.noreply.github.com>
2026-06-09 14:07:10 +0200

    Inicjalizacja projektu

---
+++ src/main/java/com/projekt/uprzejmiedonosze/UprzejmieDonoszeApplication.java
@@ -0,0 +1,13 @@
+package com.projekt.uprzejmiedonosze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UprzejmieDonoszeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UprzejmieDonoszeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projekt/uprzejmiedonosze/UprzejmieDonoszeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projekt.uprzejmiedonosze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UprzejmieDonoszeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
